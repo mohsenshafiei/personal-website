8184c58ed21a57ccd36461999861b43a6aab4a04
Mohsen Shafiei <mohsenshafieitafreshi@gmail.com>
2019-06-23 18:48:29 +0430

    feat: add who am i biography post.

--- src/biography-posts/index.js
+++ src/biography-posts/index.js
@@ -8,4 +8,51 @@ export const biographies = [
     content: 'I was born Tehran. ',
     link: '1',
   },
+  {
+    title: 'Who Am I?',
+    description: '',
+    author: 'Mohsen Shafiei Tafreshi',
+    time: '3 Minutes Reading',
+    date: '26 June 2019',
+    content: `
+      My name is Mohsen Shafiei Tafreshi and I am Frontend Engineer from Tehran,
+      Iran. I was born in Tehran on October 26, 1992, and right now I am 26.
+      Currently, I am working as a Frontend Engineer at Snapp.ir. I have started
+      programming since I was 18 and I chose that to become a frontend engineer 5 years ago.
+      Let me explain it from the begining, it was the summer of 2011 when I
+      decided to change my life. Those days I was just a normal printing house worker who had
+      finished his school and, he couldn't find any interesting thing in his life. However, I deeply knew
+      that I should do something. I really knew that I am deserved to have a better life
+      and I need 360 degree full turn in my life. At the first step ّI resigned to continue
+      my education but there was something that ّI shoud overcome and that was Iranian National Examination (Konkoor)
+      to enter Iranian univerisities. I just started to learn different sciences
+      and skills like Math, Physics, English language and etc. I was full of motivation and I really wanted
+      to achieve my dreams. After one year of trying and practicing finally, I accepted to continue my education
+      in the field of Computer Science. I went to the University of Tehran which is the best university in my country.
+      As a computer science student literally I didn't know anything about computers. I was just a normal user of computers
+      and in comparison with my classmates at the begining I was zero . I have started to learn the C++ language
+      and then Java and etc. I have practiced a lot to learn Algorithms, Data Structures and the most important thing Math.
+      It was there that I found that I am good at solving alogrithmic problems.
+      Spring of 2013 was a special season for me and I have won the university scholarship award, and, I was the teacher assistant
+      for two or more courses at the university and these little things had a big effect in my life. It was there that I found that
+      if you want to understand something deeply you should teach it to others and it was there that I learned how can I manage people.
+      Believe me handling a class of young eager minds of tomorrow is much much harder than handling teamates and developers.
+      One the most important things of my life happened when I was a senior undergraduate student and
+      it was finding the angle of life my wife Mandana. After comming of Mandana to my life everything has changed.
+      I had to be more powerful, responsible, and etc. After entering the university it was the second big challenge
+      of my life because I didn't have money, job and many other things. It was clear to me that if I really want to have
+      Mandana in my life I have to conquer another summit. I started to find a good job to earn money to build my life but my skills
+      weren't enough. I rejected more than one hundred times bud I didn't give up. I started to learn Web Development skills like Php, JavaScript,
+      HTML, CSS and etc. After learning of these skills I was really confident about myself and I was telling myself all the time: If they
+      don't give me a job I should create a job for myself and if they don't give me an opportunity I have to create it. I chose that to run my
+      startups and at the first try I developed the Desta Application which it was a SMS Spam detector but unfortunately it failed. At the second
+      try I developed the Lavilook application which it was a social shopping network and it failed because we couldn't find any investor.
+      I have failed and failed again and again but I couldn't earn any money but during this journey I have learned a lot of skills and finally I found
+      that I know nothing about being a professinal programming. Once again I have needed a change in my life and I had to chose that my career path.
+      During my searchs for finding a job I got a good offer from Varzesh3.ir which it is the most veiwed website in my country. They needed a fulltime
+      frontend developer to rewrite one of the product.
+      .
+    `,
+    link: '1',
+  },
 ];
